32a182b4eedd9e727802c97c102808fae1d5d32d
T0raT <tigerliu@bu.edu>
2024-06-01 19:50:18 -0400

    Added new stages to callbacks.js

--- server/src/callbacks.js
+++ server/src/callbacks.js
@@ -1,7 +1,11 @@
 import { ClassicListenersCollector } from "@empirica/core/admin/classic";
 export const Empirica = new ClassicListenersCollector();
 
-Empirica.onGameStart(({ game }) => {});
+Empirica.onGameStart(({ game }) => {
+  const round = game.addRound({name: `Round`,});
+  round.addStage({name: "choice", duration: 10000});
+  round.addStage({name: "result", duration: 10000});
+});
 
 Empirica.onRoundStart(({ round }) => {});
 
