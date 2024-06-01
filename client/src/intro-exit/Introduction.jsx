83942cf68ce7a0008d6ac2013c445a4bdeed8df2
T0raT <tigerliu@bu.edu>
2024-06-01 19:39:52 -0400

    Stages setup with Choice stage as initial stage

--- client/src/intro-exit/Introduction.jsx
+++ client/src/intro-exit/Introduction.jsx
@@ -5,14 +5,17 @@ export function Introduction({ next }) {
   return (
     <div className="mt-3 sm:mt-5 p-20">
       <h3 className="text-lg leading-6 font-medium text-gray-900">
-        Instruction One
+        The prisoners dilemma
       </h3>
       <div className="mt-2 mb-6">
         <p className="text-sm text-gray-500">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius aliquam
-          laudantium explicabo pariatur iste dolorem animi vitae error totam. At
-          sapiente aliquam accusamus facere veritatis.
+          In this game, you will be partnered with another experiment participant.
         </p>
+        <p className="text-sm text-gray-500">In <strong>each round of the game</strong>, you and your partner will:</p>
+        <ul className="text-sm text-grey-500 list-disc">
+          <li>Choose whether to work together or to look out for your own interests</li>
+          <li>See each other's choices and the results of your choices</li>
+        </ul>
       </div>
       <Button handleClick={next} autoFocus>
         <p>Next</p>
---
+++ client/stages/Choice.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Choice() {
+  return (
+      <div>
+        "Choice stage not yet implemented"
+      </div>
+  )
+}
