425062d8a38ed4e7b015b85a88dc98d07e945fad
T0raT <tigerliu@bu.edu>
2024-06-01 21:23:05 -0400

    Removed unocss and ensured tailwindcss setup, also added choice stage outlines

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "../node_modules/@empirica/core/dist/player.css";
 import App from "./App";
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
--- client/stages/Choice.jsx
+++ client/stages/Choice.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
-
+import Button  from 'react-bootstrap/Button';
+import { Button as EmpButton } from '../src/components/Button'
 export function Choice() {
   return (
       <div>
-        "Choice stage not yet implemented"
+        <h2>🙀 You and your partner in crime have been arrested! 🙀</h2>
+        <ul className="list-disc list-inside">
+          <li>
+            If you both 🤐🤐 keep silent, you will both get
+            <strong>2 months</strong> in jail.
+          </li>
+          <li>
+            If you 📣 testify against your partner and he 🤐 keeps silent,
+            <strong>you will only spend 1 month in jail</strong>.
+          </li>
+          <li>
+            If you 🤐 keep silent, but your partner 📣 testifies, you get
+            <strong>12 months</strong> in jail.
+          </li>
+          <li>
+            If you both 📣📣 testify, you both get <strong>6 months</strong> in
+            jail.
+          </li>
+        </ul>
+        <br/>
+        <p>What do you do?</p>
+        <div className="flex justify-center">
+          <EmpButton className="m-5">🤐 Keep silent</EmpButton>
+          <EmpButton className="m-5">📣 Testify</EmpButton>
+        </div>
       </div>
-  )
+
+  );
 }
